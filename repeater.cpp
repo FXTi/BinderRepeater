8fefdef447fd24c0dade009b5965079b9452ff5f
FXTi <zjxiang1998@gmail.com>
2019-08-20 17:24:09 +0800

    try

--- repeater.cpp
+++ repeater.cpp
@@ -68,164 +68,6 @@ void assert_fail(const char *file, int line, const char *func, const char *expr)
 // Where to print the parcel contents: aout, alog, aerr. alog doesn't seem to work.
 #define PLOG aout
 
-
-
-// Interface (our AIDL) - Shared by server and client
-class IDemo : public IInterface {
-    public:
-        enum {
-            ALERT = IBinder::FIRST_CALL_TRANSACTION,
-            PUSH,
-            ADD
-        };
-        // Sends a user-provided value to the service
-        virtual void        push(int32_t data)          = 0;
-        // Sends a fixed alert string to the service
-        virtual void        alert()                     = 0;
-        // Requests the service to perform an addition and return the result
-        virtual int32_t     add(int32_t v1, int32_t v2) = 0;
-
-        DECLARE_META_INTERFACE(Demo);  // Expands to 5 lines below:
-        //static const android::String16 descriptor;
-        //static android::sp<IDemo> asInterface(const android::sp<android::IBinder>& obj);
-        //virtual const android::String16& getInterfaceDescriptor() const;
-        //IDemo();
-        //virtual ~IDemo();
-};
-
-// Client
-class BpDemo : public BpInterface<IDemo> {
-    public:
-        BpDemo(const sp<IBinder>& impl) : BpInterface<IDemo>(impl) {
-            ALOGD("BpDemo::BpDemo()");
-        }
-
-        virtual void push(int32_t push_data) {
-            Parcel data, reply;
-            data.writeInterfaceToken(IDemo::getInterfaceDescriptor());
-            data.writeInt32(push_data);
-
-            aout << "BpDemo::push parcel to be sent:\n";
-            data.print(PLOG); endl(PLOG);
-
-            remote()->transact(PUSH, data, &reply);
-
-            aout << "BpDemo::push parcel reply:\n";
-            reply.print(PLOG); endl(PLOG);
-
-            ALOGD("BpDemo::push(%i)", push_data);
-        }
-
-        virtual void alert() {
-            Parcel data, reply;
-            data.writeInterfaceToken(IDemo::getInterfaceDescriptor());
-            data.writeString16(String16("The alert string"));
-            remote()->transact(ALERT, data, &reply, IBinder::FLAG_ONEWAY);    // asynchronous call
-            ALOGD("BpDemo::alert()");
-        }
-
-        virtual int32_t add(int32_t v1, int32_t v2) {
-            Parcel data, reply;
-            data.writeInterfaceToken(IDemo::getInterfaceDescriptor());
-            data.writeInt32(v1);
-            data.writeInt32(v2);
-            aout << "BpDemo::add parcel to be sent:\n";
-            data.print(PLOG); endl(PLOG);
-            remote()->transact(ADD, data, &reply);
-            ALOGD("BpDemo::add transact reply");
-            reply.print(PLOG); endl(PLOG);
-
-            int32_t res;
-            status_t status = reply.readInt32(&res);
-            ALOGD("BpDemo::add(%i, %i) = %i (status: %i)", v1, v2, res, status);
-            return res;
-        }
-};
-
-    //IMPLEMENT_META_INTERFACE(Demo, "Demo");
-    // Macro above expands to code below. Doing it by hand so we can log ctor and destructor calls.
-    const android::String16 IDemo::descriptor("Demo");
-    const android::String16& IDemo::getInterfaceDescriptor() const {
-        return IDemo::descriptor;
-    }
-    android::sp<IDemo> IDemo::asInterface(const android::sp<android::IBinder>& obj) {
-        android::sp<IDemo> intr;
-        if (obj != NULL) {
-            intr = static_cast<IDemo*>(obj->queryLocalInterface(IDemo::descriptor).get());
-            if (intr == NULL) {
-                intr = new BpDemo(obj);
-            }
-        }
-        return intr;
-    }
-    IDemo::IDemo() { ALOGD("IDemo::IDemo()"); }
-    IDemo::~IDemo() { ALOGD("IDemo::~IDemo()"); }
-    // End of macro expansion
-
-// Server
-class BnDemo : public BnInterface<IDemo> {
-    virtual status_t onTransact(uint32_t code, const Parcel& data, Parcel* reply, uint32_t flags = 0);
-};
-
-status_t BnDemo::onTransact(uint32_t code, const Parcel& data, Parcel* reply, uint32_t flags) {
-    ALOGD("BnDemo::onTransact(%i) %i", code, flags);
-    data.checkInterface(this);
-    data.print(PLOG); endl(PLOG);
-
-    switch(code) {
-        case ALERT: {
-            alert();    // Ignoring the fixed alert string
-            return NO_ERROR;
-        } break;
-        case PUSH: {
-            int32_t inData = data.readInt32();
-            ALOGD("BnDemo::onTransact got %i", inData);
-            push(inData);
-            ASSERT(reply != 0);
-            reply->print(PLOG); endl(PLOG);
-            return NO_ERROR;
-        } break;
-        case ADD: {
-            int32_t inV1 = data.readInt32();
-            int32_t inV2 = data.readInt32();
-            int32_t sum = add(inV1, inV2);
-            ALOGD("BnDemo::onTransact add(%i, %i) = %i", inV1, inV2, sum);
-            ASSERT(reply != 0);
-            reply->print(PLOG); endl(PLOG);
-            reply->writeInt32(sum);
-            return NO_ERROR;
-        } break;
-        default:
-            return BBinder::onTransact(code, data, reply, flags);
-    }
-}
-
-class Demo : public BnDemo {
-    virtual void push(int32_t data) {
-        INFO("Demo::push(%i)", data);
-    }
-    virtual void alert() {
-        INFO("Demo::alert()");
-    }
-    virtual int32_t add(int32_t v1, int32_t v2) {
-        INFO("Demo::add(%i, %i)", v1, v2);
-        return v1 + v2;
-    }
-};
-
-
-// Helper function to get a hold of the "Demo" service.
-sp<IDemo> getDemoServ() {
-    sp<IServiceManager> sm = defaultServiceManager();
-    ASSERT(sm != 0);
-    sp<IBinder> binder = sm->getService(String16("Demo"));
-    // TODO: If the "Demo" service is not running, getService times out and binder == 0.
-    ASSERT(binder != 0);
-    sp<IDemo> demo = interface_cast<IDemo>(binder);
-    ASSERT(demo != 0);
-    return demo;
-}
-
 const std::string base64_chars = 
              "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
              "abcdefghijklmnopqrstuvwxyz"
@@ -272,17 +114,53 @@ std::string base64_decode(std::string const& encoded_string) {
   return ret;
 }
 
+//Please keep sync with official implementation
+struct Parcel_model {
+    status_t            mError;
+    uint8_t*            mData;  //useful
+    size_t              mDataSize;  //useful
+    size_t              mDataCapacity;
+    mutable size_t      mDataPos;
+    uint64_t*               mObjects;   //useful
+    size_t              mObjectsSize;   //useful
+    size_t              mObjectsCapacity;
+    mutable size_t      mNextObjectHint;
+    mutable bool        mObjectsSorted;
+
+    mutable bool        mFdsKnown;
+    mutable bool        mHasFds;
+    bool                mAllowFds;
+
+    void*               mOwner;
+    void*               mOwnerCookie;
+
+    Parcel_model(char *data, size_t Dsize, uint64_t mobjects, size_t Osize): 
+        mError(0), mData(0), mDataSize(Dsize),
+        mDataCapacity(0), mDataPos(0), mObjects(malloc()), mObjectsSize(Osize),
+        mObjectsCapacity(0), mNextObjectHint(0), mObjectsSorted(0), mFdsKnown(0),
+        mHasFds(0), mAllowFds(0), mOwner(0), mOwnerCookie(0) { 
+            string res = base64_decode(string(data));
+            char *resc = res.c_str();
+            mData = malloc(Dsize);
+            memcpy(mData, resc, Dsize);
+            mObjects = malloc(uint64_t);
+            *mObjects = mobjects;
+    }
+};
+
 int main(int argc, char **argv) {
+    printf("%d\n", sizeof(Parcel));
     if (argc == 5) {
         sp<IServiceManager> sm = defaultServiceManager();
         ASSERT(sm != 0);
         sp<IBinder> binder = sm->getService(String16(argv[1]));
         // TODO: If the "Demo" service is not running, getService times out and binder == 0.
         ASSERT(binder != 0);
-        Parcel data, reply;
-        binder->transact(atoll(argv[2]), data, &reply, atoll(argv[4]));
+        Parcel reply;
+        Parcel_model data(argv[4], atoll(argv[5]), atoll(argv[6]), atoll(argv[6]));
+        binder->transact(atoll(argv[2]), (Parcel)data, &reply, atoll(argv[3]));
     } else {
-        INFO("%s service_name cmd base64(data) flag", argv[0]);
+        INFO("%s service_name cmd flag base64(data) mDataSize mObjects mObjectsSize", argv[0]);
     }
 
     return 0;
